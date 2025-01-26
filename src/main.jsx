8c528ee07fd093d3e52da2c3b61796ccc262eac2
Ankit Rathore <ankitrathore7847@gmail.com>
2025-01-26 19:57:03 +0530

    Navbar & Footer setup

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,7 @@ import PricePage from './landing_page/pricing/PricingPage'
 import ProductPage from './landing_page/products/ProductPage'
 import Signup from './landing_page/signup/Signup'
 import Support from './landing_page/support/Support'
-
-// import './index.css'
-
+import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <div>
