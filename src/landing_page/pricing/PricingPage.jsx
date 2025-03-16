6f9855ac6183cf87e239eeebbea65b3c37a01b1b
Ankit Rathore <ankitrathore7847@gmail.com>
2025-03-16 16:08:39 +0530

    complete about section

--- src/landing_page/pricing/PricingPage.jsx
+++ src/landing_page/pricing/PricingPage.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import Hero from './Hero';
 import Brokerage from './Brokerage';
-import Navbar from '../Navbar';
-import Footer from '../Footer';
+
+
 
 function PricePage() {
     return ( 
         <>
-        <Navbar/>
+       
          <Brokerage/>
          <Hero/>
-         <Footer/>
+        
          
         </>
        
--- src/landing_page/products/ProductPage.jsx
+++ src/landing_page/products/ProductPage.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import LeftSection from './LeftSection';
 import RightPage from './RightPage';
 import Universe from './Universe';
-import Navbar from '../Navbar';
-import Footer from '../Footer';
 import Hero from './Hero';
 function ProductPage() {
     return ( 
