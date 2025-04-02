b4fdc6542b889c9d2aa1e1d1812307dce6f4da71
Ankit Rathore <ankitrathore7847@gmail.com>
2025-04-02 14:41:32 +0530

    creating left section for product tab

--- src/landing_page/products/ProductPage.jsx
+++ src/landing_page/products/ProductPage.jsx
@@ -7,7 +7,7 @@ function ProductPage() {
     return ( 
         <>
         <Hero/>
-        <LeftSection/>
+        <LeftSection  imageURL="media/images/kite.png" productName="Kite" productDescripton="Our ultra-fast flagship trading platform with streaming market data, advanced charts, an elegant UI, and more. Enjoy the Kite experience seamlessly on your Android and iOS devices." tryDemo="" learnMore="" googlePlay="" appStore="" />
         <RightPage/>
         <Universe/>
         
