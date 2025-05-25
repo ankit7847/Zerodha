5b9b0d8160195e3bb9957388a6fb89e8718a4714
Ankit Rathore <ankitrathore7847@gmail.com>
2025-05-25 09:47:29 +0530

    creating support page

--- src/landing_page/support/Hero.jsx
+++ src/landing_page/support/Hero.jsx
@@ -2,9 +2,12 @@ import React from 'react';
 
 function Hero() {
     return ( 
-        <h1>
-            Hero
-        </h1>
+        <section className="container-fluid " id="supportHero">
+            <div className="row p-4 mb-5 mt-5" id="supportwrapper">
+                <h3>Support Portal</h3>
+                <a href="">Track Ticket</a>
+            </div>
+        </section>
      );
 }
 
