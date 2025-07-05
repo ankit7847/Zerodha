3887e1ae03b33e657dd0f09e56a071a5ad670891
Ankit Rathore <ankitrathore7847@gmail.com>
2025-07-05 11:21:17 +0530

    first commit

---
+++ backend/models/PositionsOrder.js
@@ -0,0 +1,8 @@
+const {model} = require("mongoose");
+
+
+const PositionsSchema = require("../schemas/PositionsSchema");
+
+const  PositionsOrder =  model("Position",PositionsSchema);
+
+module.exports = PositionsOrder;
